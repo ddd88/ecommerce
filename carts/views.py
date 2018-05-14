4da06d2eb24665c6b0b16a9abe21968680c02988
Dale <dalewalk10@gmail.com>
2018-05-14 21:54:12 +1000

    carts update

--- carts/views.py
+++ carts/views.py
@@ -1,29 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-
+from products.models import Product
 from .models import Cart
-# Create your views here.
 
-
-def cart_create(user=None):
-    cart_obj = Cart.objects.create(user=None)
-    print('New cart created')
-    return cart_obj
 
 def cart_home(request):
-    cart_id = request.session.get("cart_id", None)
-    # if cart_id is None: # and isinstance(cart_id, int):
-    #     cart_create()
-    #     request.session['cart_id'] = cart_obj.id
-    # else:
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        print('Cart ID exists')
-        cart_obj = qs.first()
-        if request.user.is_authenticated() and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_obj.id
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = cart_obj.products.all()
+    total = 0
+    for x in products:
+        total += x.price
+    print(total)
+    cart_obj.total = total
+    cart_obj.save()
     return render(request, "carts/home.html", {})
+
+
+def cart_update(request):
+    product_id = 1
+    product_obj = Product.objects.get(id=product_id)
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    if product_obj in cart_obj.products.all():
+        cart_obj.products.remove(product_obj)
+    else:
+        cart_obj.products.add(product_obj)
+    return redirect("cart:home")
