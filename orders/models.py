8d2499d3940b822569fccbe577de5fe6cdfb3e63
Dale <dalewalk10@gmail.com>
2018-05-20 12:01:12 +1000

    Choose billing and shipping addresses and Checkout success

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 
+from addresses.models import Address
 from billing.models import BillingProfile
 from carts.models import Cart
 from decimal import *
@@ -17,7 +18,12 @@ ORDER_STATUS_CHOICES = (
 class OrderManager(models.Manager):
     def new_or_get(self, billing_profile, cart_obj):
         created = False
-        qs = self.get_queryset().filter(billing_profile=billing_profile, cart=cart_obj, active=True)
+        qs = self.get_queryset().filter(
+            billing_profile=billing_profile,
+            cart=cart_obj,
+            active=True,
+            status='created'
+        )
         if qs.count() == 1:
             obj = qs.first()
         else:
@@ -27,13 +33,15 @@ class OrderManager(models.Manager):
 
 
 class Order(models.Model):
-    billing_profile = models.ForeignKey(BillingProfile, null=True)
-    order_id        = models.CharField(max_length=120, blank=True)
-    cart            = models.ForeignKey(Cart)
-    status          = models.CharField(max_length=120, default='created')
-    shipping_total  = models.DecimalField(default=Decimal(15.00), max_digits=100, decimal_places=2)
-    total           = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
-    active          = models.BooleanField(default=True)
+    billing_profile     = models.ForeignKey(BillingProfile, null=True, blank=True)
+    order_id            = models.CharField(max_length=120, blank=True)
+    shipping_address    = models.ForeignKey(Address, related_name="shipping_address", null=True, blank=True)
+    billing_address     = models.ForeignKey(Address, related_name="billing_address", null=True, blank=True)
+    cart                = models.ForeignKey(Cart)
+    status              = models.CharField(max_length=120, default='created')
+    shipping_total      = models.DecimalField(default=Decimal(15.00), max_digits=100, decimal_places=2)
+    total               = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    active              = models.BooleanField(default=True)
 
     def __str__(self):
         return self.order_id
@@ -41,12 +49,27 @@ class Order(models.Model):
     objects = OrderManager()
 
     def update_total(self):
-        cart_total = self.cart.total
-        shipping_total = self.shipping_total
-        new_total = cart_total + shipping_total
-        self.total = new_total
+        cart_total      = self.cart.total
+        shipping_total  = self.shipping_total
+        new_total       = cart_total + shipping_total
+        self.total      = new_total
         self.save()
         return new_total
+
+    def check_done(self):
+        billing_profile     = self.billing_profile
+        shipping_address    = self.shipping_address
+        billing_address     = self.billing_address
+        total               = self.total
+        if billing_profile and shipping_address and billing_address and total > 0:
+            return True
+        return False
+
+    def mark_paid(self):
+        if self.check_done():
+            self.status = "paid"
+            self.save()
+        return self.status
 
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
@@ -61,10 +84,10 @@ pre_save.connect(pre_save_create_order_id, sender=Order)
 
 def post_save_cart_total(sender, instance, created, *args, **kwargs):
     if not created:
-        cart_obj = instance
-        cart_total = cart_obj.total
-        cart_id = cart_obj.id
-        qs = Order.objects.filter(cart__id=cart_id)
+        cart_obj    = instance
+        cart_total  = cart_obj.total
+        cart_id     = cart_obj.id
+        qs          = Order.objects.filter(cart__id=cart_id)
         if qs.count() == 1:
             order_obj = qs.first()
             order_obj.update_total()
